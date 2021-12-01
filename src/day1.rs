511477ed3df93cf640abe32e4896f5a4dd807ec6
Zoé Cassiopée Gauthier <hello@blorp.dev>
2021-12-01 14:48:07 -0500

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,37 @@
+use std::io::{self, BufRead};
+
+fn increases(measurements: &Vec<u64>) -> u64 {
+    measurements
+        .windows(2)
+        .fold(0, |acc, x| if x[1] > x[0] { acc + 1 } else { acc })
+}
+
+fn convolve(measurements: &Vec<u64>) -> Vec<u64> {
+    measurements.windows(3).map(|x| x.iter().sum()).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    const INPUT: &'static str = include_str!("../inputs/day1.txt");
+    let measurements: Vec<u64> = io::Cursor::new(INPUT)
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect();
+
+    println!("increased {} times", increases(&measurements));
+    println!(
+        "increased {} times",
+        increases(&convolve(&measurements))
+    );
+}
+
+#[test]
+fn part1_example() {
+    let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    assert_eq!(increases(&measurements), 7);
+}
+
+#[test]
+fn part2_example() {
+    let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    assert_eq!(increases(&convolve(&measurements)), 5);
 }
